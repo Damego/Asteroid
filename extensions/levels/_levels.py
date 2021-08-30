4ceee1ca6d7a81a16ad3a9ddbba4b85722c38bdd
Damego <danyabatueff@gmail.com>
2021-08-30 21:37:10 +0500

    Update _levels.py

--- extensions/levels/_levels.py
+++ extensions/levels/_levels.py
@@ -8,7 +8,9 @@ last_user_message = {}
 
 async def update_member(bot, member_or_message, exp):
     """Method for giving exp for guild member"""
-    guild_id = str(member_or_message.guild.id)
+    guild = member_or_message.guild
+    guild_id = str(guild.id)
+    message = None
 
     if isinstance(member_or_message, discord.Message):
         message = member_or_message
@@ -18,7 +20,7 @@ async def update_member(bot, member_or_message, exp):
     elif isinstance(member_or_message, discord.Member):
         member = member_or_message
 
-    guild_users_collection = bot.get_guild_users_collection(member.guild.id)
+    guild_users_collection = bot.get_guild_users_collection(guild_id)
     member_stats = guild_users_collection.find_one({'_id':str(member.id)})['leveling']
 
     xp = member_stats['xp'] + exp
@@ -26,12 +28,13 @@ async def update_member(bot, member_or_message, exp):
     level = member_stats['level']
 
     guild_users_collection.update_one(
-        {'_id':str(member.id)},
-        {'$set':{'leveling.xp':xp}})
-
-    guild_users_collection.update_one(
-        {'_id':str(member.id)},
-        {'$set':{'leveling.xp_amount':xp_amount}})
+            {'_id':str(member.id)},
+            {'$set':{
+                'leveling.xp':xp,
+                'leveling.xp_amount':xp_amount
+                }
+            }
+        )
 
     exp_to_next_level = formula_of_experience(member_stats['level'])
     while xp > exp_to_next_level:
@@ -39,25 +42,25 @@ async def update_member(bot, member_or_message, exp):
         xp -= exp_to_next_level
 
         guild_users_collection.update_one(
-        {'_id':str(member.id)},
-        {'$set':{'leveling.level':level}})
-
-        guild_users_collection.update_one(
-        {'_id':str(member.id)},
-        {'$set':{'leveling.xp':xp}})
-
+            {'_id':str(member.id)},
+            {'$set':{
+                'leveling.level':level,
+                'leveling.xp':xp
+                }
+            }
+        )
 
         exp_to_next_level = formula_of_experience(level)
 
-        guild_level_roles_collection = bot.get_guild_level_roles_collection(member.guild.id)
+        guild_level_roles_collection = bot.get_guild_level_roles_collection(guild_id)
         role_id = guild_level_roles_collection.find_one({'_id':(str(level))})
-        
+        if role_id is None:
+            continue
 
-        if role_id is not None:
-            new_role = member.guild.get_role(role_id)
-            if new_role is not None:
-                await update_member_role(guild_id, member, new_role)
-                await notify_member(arg.guild, member, member_stats['level'], new_role)
+        new_role = guild.get_role(role_id)
+        if new_role is not None:
+            await update_member_role(bot, guild_id, member, new_role)
+            await notify_member(guild, member, member_stats['level'], new_role, message)
 
 
 def formula_of_experience(level:int):
@@ -81,8 +84,8 @@ def check_timeout(guild_id, member):
     return False
 
 
-async def update_member_role(guild_id, member, new_role):
-    guild_users_collection = get_guild_users_collection(guild_id)
+async def update_member_role(bot, guild_id, member, new_role):
+    guild_users_collection = bot.get_guild_users_collection(guild_id)
     member_stats = guild_users_collection.find_one({'_id':str(member.id)})['leveling']
     old_role = member_stats['role']
 
@@ -97,10 +100,11 @@ async def update_member_role(guild_id, member, new_role):
         )
 
 
-async def notify_member(guild, member, new_level, new_role=None):
+async def notify_member(guild, member, new_level, new_role=None, message:discord.Message=None):
     system_channel = guild.system_channel
-    if new_role:
-        if system_channel:
-            await system_channel.send(f'{member.mention} получил `{new_level}-й` уровень и повышение до {new_role.mention}', delete_after=15)
-        else:
-            await member.send(f'Вы получили `{new_level}-й` уровень и повышение до {str(new_role)}', delete_after=15)
+    if message is not None:
+        await message.channel.send(f'Вы получили `{new_level}-й` уровень и повышение до {str(new_role)}', delete_after=15)
+    elif system_channel is not None:
+        await system_channel.send(f'{member.mention} получил `{new_level}-й` уровень и повышение до {new_role.mention}', delete_after=15)
+    else:
+        await member.send(f'Вы получили `{new_level}-й` уровень и повышение до {new_role}', delete_after=15)
