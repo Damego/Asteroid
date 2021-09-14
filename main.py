c357faf613e341485a84cdf47557ade854c11713
Damego <danyabatueff@gmail.com>
2021-09-14 09:00:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ async def on_command_error(ctx:commands.Context, error):
         desc = f'**You not have permission for this!**\nRequired permissions: `{", ".join(error.missing_perms)}`'
     elif isinstance(error, commands.CommandNotFound):
         desc = 'Command not found!'
-    elif isinstance(error, discord.Forbidden):
+    elif isinstance(error, discord.errors.Forbidden):
         desc = '**Bot not have permission for this!**'
     else:
         desc = f"""
