da769b3fc46832b4a69447e7c3e878b963290acf
Damego <danyabatueff@gmail.com>
2021-10-06 21:42:17 +0500

    Testing deploy №7

--- cogs/settings.py
+++ cogs/settings.py
@@ -194,9 +194,9 @@ class Settings(Cog):
         embed = discord.Embed(title='Перезагрузка...', color=0x2f3136)
         await message.edit(embed=embed)
         try:
-            #os.execv(sys.executable, ['python'] + sys.argv)
-            os.system('python3 /home/ubuntu/asteroid_bot/main.py')
-            exit(1)
+            os.execv(sys.executable, ['python3.9'] + sys.argv)
+            #os.system('python3.9 /home/ubuntu/asteroid_bot/main.py')
+            #exit(1)
         except Exception as e:
             print('CANT RESTART BOT. ERROR', e)
             print('Reloading extensions...')
