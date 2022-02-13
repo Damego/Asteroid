fc3f744344f4fe1cc7aebda1fa075cebe28f4a32
Damego <danyabatueff@gmail.com>
2022-02-13 22:31:47 +0500

    chore: updated discord applications

--- cogs/fun/main.py
+++ cogs/fun/main.py
@@ -38,18 +38,20 @@ bored_api_types = [
     "busywork",
 ]
 discord_activities_list = {
-    "YouTube": "880218394199220334",
+    "Watch Together": "880218394199220334",
     "Betrayal.io": "773336526917861400",
     "Fishington.io": "814288819477020702",
-    "Poker Night": "755827207812677713",
-    "Chess": "832012774040141894",
-    "Word Snack": "879863976006127627",
-    "Letter Tile": "879863686565621790",
-    "Doodle Crew": "878067389634314250",
     "awkword": "879863881349087252",
-    "spellcast": "852509694341283871",
-    "checkers": "832013003968348200",
-    "puttparty": "763133495793942528",
+    "PuttParty": "763133495793942528",
+    "Word Snacks": "879863976006127627",
+    "Sketch Heads": "902271654783242291",
+    "Poker Night (Boost Level 1)": "755827207812677713",
+    "Chess In The Park (Boost Level 1)": "832012774040141894",
+    "Checkers In The Park (Boost Level 1)": "832013003968348200",
+    "Letter Tile (Boost Level 1)": "879863686565621790",
+    "SpellCast (Boost Level 1)": "852509694341283871",
+    "Ocho (Boost Level 1)": "832025144389533716",
+    "Doodle Crew (Old)": "878067389634314250",
 }
 
 
