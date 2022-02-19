71ebec884de91220b63395b1c3aef88d26f8e53f
Damego <danyabatueff@gmail.com>
2022-02-19 15:20:13 +0500

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -233,7 +233,7 @@ class Music(Cog):
         ]
         embed = Embed(
             title=content["CURRENT_QUEUE_TITLE_TEXT"],
-            description=f"**{content['CURRENT_SONG_TEXT']}:** `{player.current.name}`"
+            description=f"**{content['CURRENT_SONG_TEXT']}:** `{player.current.title}`"
             "\n{'\n'.join(tracks)}",
             color=guild_data.configuration.embed_color,
         )
