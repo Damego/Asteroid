b87a1fad9cf218d51ab632166670ec7b7b1fdb2f
Damego <danyabatueff@gmail.com>
2022-02-26 21:49:54 +0500

    refactor: removed autocomplete from tag add

--- cogs/tags.py
+++ cogs/tags.py
@@ -95,8 +95,7 @@ class Tags(Cog):
                 name="tag_name",
                 description="The name of tag",
                 option_type=SlashCommandOptionType.STRING,
-                required=True,
-                autocomplete=True,
+                required=True
             ),
             create_option(
                 name="type",
