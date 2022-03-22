3d1eaccd14cf7af36165f43c57f3f891816856a9
Damego <danyabatueff@gmail.com>
2022-03-22 22:20:09 +0500

    fix!: One more music fix

--- cogs/music.py
+++ cogs/music.py
@@ -41,21 +41,22 @@ class LavalinkVoiceClient(VoiceClient):
     def __init__(self, client, channel: VoiceChannel):
         self.client = client
         self.channel = channel
+        self.lavalink = client.lavalink
 
     async def on_voice_server_update(self, data):
         lavalink_data = {"t": "VOICE_SERVER_UPDATE", "d": data}
-        await self.voice_update_handler(lavalink_data)
+        await self.lavalink.voice_update_handler(lavalink_data)
 
     async def on_voice_state_update(self, data):
         lavalink_data = {"t": "VOICE_STATE_UPDATE", "d": data}
-        await self.voice_update_handler(lavalink_data)
+        await self.lavalink.voice_update_handler(lavalink_data)
 
     async def connect(self, *, timeout: float, reconnect: bool) -> None:
         """
         Connect the bot to the voice channel and create a player_manager
         if it doesn't exist yet.
         """
-        self.player_manager.create(guild_id=self.channel.guild.id)
+        self.lavalink.player_manager.create(guild_id=self.channel.guild.id)
         await self.channel.guild.change_voice_state(channel=self.channel)
 
     async def disconnect(self, *, force: bool) -> None:
@@ -63,7 +64,7 @@ class LavalinkVoiceClient(VoiceClient):
         Handles the disconnect.
         Cleans up running player and leaves the voice client.
         """
-        player = self.player_manager.get(self.channel.guild.id)
+        player = self.lavalink.player_manager.get(self.channel.guild.id)
         if not force and not player.is_connected:
             return
         await self.channel.guild.change_voice_state(channel=None)
