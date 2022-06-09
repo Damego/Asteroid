4814c629164a75fdab4918239256f3473088cef3
Damego <danyabatueff@gmail.com>
2022-06-09 11:58:27 +0500

    refactor(autorole): add try except block in fetch message

--- source/cogs/auto_role.py
+++ source/cogs/auto_role.py
@@ -748,7 +748,10 @@ class AutoRole(Cog):
 
         channel_id = autorole.channel_id
         channel: TextChannel = ctx.guild.get_channel(channel_id)
-        original_message: ComponentMessage = await channel.fetch_message(int(autorole.message_id))
+        try:
+            original_message: ComponentMessage = await channel.fetch_message(int(autorole.message_id))
+        except Exception:
+            raise errors.AutoRoleNotFound
         # if not original_message.components: # ? Why I write that?
         #    raise errors.MessageWithoutAutoRole
         return autorole, original_message
