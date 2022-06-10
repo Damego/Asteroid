c82f14ddfd9951338b6ed73d9d2ed668fd7e2019
Damego <danyabatueff@gmail.com>
2022-06-10 15:25:41 +0500

    Add new properties to tags

--- source/utils/db/models/tag.py
+++ source/utils/db/models/tag.py
@@ -12,12 +12,18 @@ class GuildTag(DictMixin):
         "is_embed",
         "title",
         "description",
+        "created_at",
+        "last_edited_at",
+        "uses_count",
     )
     name: str
     author_id: int
     is_embed: bool
     title: str
     description: str
+    created_at: int
+    last_edited_at: int
+    uses_count: int
 
     def __init__(self, _request: RequestClient, guild_id: int, **kwargs) -> None:
         self._request = _request.tags
@@ -32,6 +38,9 @@ class GuildTag(DictMixin):
         is_embed: bool
         title: str
         description: str
+        created_at: int
+        last_edited_at: int
+        uses_count: int
         """
         await self._request.modify(self.guild_id, self.name, **kwargs)
         for kwarg, value in kwargs.items():
