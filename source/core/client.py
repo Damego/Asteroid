51a1d8aea14df9451fd8c931f11b42cac6707314
Damego <danyabatueff@gmail.com>
2022-07-19 11:42:21 +0500

    asteroid class

---
+++ source/core/client.py
@@ -0,0 +1,11 @@
+from interactions import Client
+
+from .database import DataBaseClient
+
+__all__ = ["Asteroid"]
+
+
+class Asteroid(Client):
+    def __init__(self, bot_token: str, mongodb_url: str, **kwargs):
+        super().__init__(bot_token, **kwargs)
+        self._database = DataBaseClient(mongodb_url)
---
+++ source/core/database/__init__.py
@@ -0,0 +1,2 @@
+from .client import DataBaseClient  # noqa
+from .models import *  # noqa
