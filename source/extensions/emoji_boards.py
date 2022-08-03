0e2d8505d99b820953b34b9c1bdb2193b4b804bc
Damego <danyabatueff@gmail.com>
2022-08-03 15:26:12 +0500

    chore: add todos for emoji boards

--- source/extensions/emoji_boards.py
+++ source/extensions/emoji_boards.py
@@ -17,6 +17,12 @@ from interactions import extension_command as command
 from interactions import extension_listener as listener
 from interactions import option
 
+# TODO:
+#   Make UI
+#   Autocomplete
+#   Exceptions
+#   Locale
+
 
 class EmojiBoards(Extension):
     def __init__(self, client):
