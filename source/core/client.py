dcf0014f1ff83ba20c9f346be4fe54aa973e785d
Damego <danyabatueff@gmail.com>
2022-09-09 20:17:46 +0500

    feat: add helpers for `Asteroid` & inherit from `VoiceChannel`

--- source/core/client.py
+++ source/core/client.py
@@ -1,4 +1,5 @@
-from interactions import Client, Snowflake, get
+from interactions import Channel, Guild, Member, Message, Role, Snowflake, User, get
+from interactions.ext.lavalink import VoiceClient
 
 from .database import DataBaseClient
 from .locale import Locale, LocaleManager
@@ -6,7 +7,7 @@ from .locale import Locale, LocaleManager
 __all__ = ["Asteroid"]
 
 
-class Asteroid(Client):
+class Asteroid(VoiceClient):
     def __init__(self, bot_token: str, mongodb_url: str, **kwargs):
         super().__init__(bot_token, **kwargs)
         self.database = DataBaseClient(mongodb_url)
@@ -20,3 +21,24 @@ class Asteroid(Client):
 
     async def get(self, *args, **kwargs):
         return await get(self, *args, **kwargs)
+
+    # Aliases to `get` function
+    async def get_guild(self, guild_id: int | Snowflake) -> Guild:
+        return await get(self, Guild, object_id=int(guild_id))
+
+    async def get_channel(self, channel_id: int | Snowflake) -> Channel:
+        return await get(self, Channel, object_id=int(channel_id))
+
+    async def get_role(self, guild_id: int | Snowflake, role_id: int | Snowflake) -> Role:
+        return await get(self, Role, parent_id=int(guild_id), object_id=int(role_id))
+
+    async def get_message(
+        self, channel_id: int | Snowflake, message_id: int | Snowflake
+    ) -> Message:
+        return await get(self, Message, parent_id=int(channel_id), object_id=int(message_id))
+
+    async def get_user(self, user_id: int | Snowflake) -> User:
+        return await get(self, User, object_id=int(user_id))
+
+    async def get_member(self, guild_id: int | Snowflake, member_id: int | Snowflake) -> Member:
+        return await get(self, Member, parent_id=int(guild_id), object_id=int(member_id))
