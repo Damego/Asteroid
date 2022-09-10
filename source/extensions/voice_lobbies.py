9590fa62625a4ecab975502e7fae8147642e6cd3
Damego <danyabatueff@gmail.com>
2022-09-10 15:48:14 +0500

    refactor: improve voice lobbies logic

--- source/extensions/voice_lobbies.py
+++ source/extensions/voice_lobbies.py
@@ -1,9 +1,10 @@
-from core import Asteroid
 from interactions import ChannelType, CommandContext, Extension, Overwrite, Permissions
 from interactions import extension_command as command
 from interactions import extension_listener as listener
 from interactions import option
 from interactions.ext.lavalink import VoiceState
+
+from core import Asteroid, GuildData  # isort: skip
 
 
 class VoiceLobbies(Extension):
@@ -19,19 +20,23 @@ class VoiceLobbies(Extension):
         if not guild_data.voice_lobbies:
             return
         if after.channel_id == guild_data.voice_lobbies.voice_channel_id:
+            if before and before.channel_id:
+                # Rejoined to main channel from own channel. For what lol?
+                await self.__check_channel(before, after, guild_data)
+
             guild = await after.get_guild()
             member = await self.client.get_member(after.guild_id, after.user_id)
 
             permissions = [
                 Overwrite(
                     id=int(member.id),
-                    type=0,
+                    type=1,
                     allow=Permissions.MANAGE_CHANNELS | Permissions.MOVE_MEMBERS,
                 )
             ]
             if guild_data.voice_lobbies.private_lobbies:
                 permissions.append(
-                    Overwrite(id=int(guild.id), type=1, deny=Permissions.VIEW_CHANNEL)
+                    Overwrite(id=int(guild.id), type=0, deny=Permissions.VIEW_CHANNEL)
                 )
             channel = await guild.create_channel(
                 f"{member.name}'s channel",
@@ -44,21 +49,43 @@ class VoiceLobbies(Extension):
             await guild_data.voice_lobbies.update()
             return
 
-        user_voice_channel = guild_data.voice_lobbies.get_lobby(owner_id=int(after.user_id))
-        if not user_voice_channel:
+        await self.__check_channel(before, after, guild_data)
+
+    async def __check_channel(self, before: VoiceState, after: VoiceState, guild_data: GuildData):
+        """
+        Removes previous user channel or takes ownership to another channel member, if channel exists.
+        """
+        lobby = guild_data.voice_lobbies.get_lobby(owner_id=int(after.user_id))
+        if not lobby:
             return
-        if before and before.channel_id == user_voice_channel.channel_id:
+        if before and before.channel_id == lobby.channel_id:
             channel = await self.client.get_channel(before.channel_id)
             if not channel.voice_states:
                 await channel.delete()
                 guild_data.voice_lobbies.remove_lobby(int(before.channel_id))
                 await guild_data.voice_lobbies.update()
                 return
-            first_member: VoiceState = channel.voice_states[0]
-            for lobby in guild_data.voice_lobbies.active_channels:
-                if lobby.channel_id == before.channel_id:
-                    lobby.owner_id = first_member.user_id
+            first_voice_state: VoiceState = channel.voice_states[0]
+            lobby.owner_id = int(first_voice_state.user_id)
+            permissions = [
+                Overwrite(
+                    id=int(after.user_id),
+                    type=1,
+                    deny=Permissions.MANAGE_CHANNELS | Permissions.MOVE_MEMBERS,
+                ),
+                Overwrite(
+                    id=int(first_voice_state.user_id),
+                    type=1,
+                    allow=Permissions.MANAGE_CHANNELS | Permissions.MOVE_MEMBERS,
+                ),
+            ]
+            for permission in channel.permission_overwrites:
+                if permission.id == after.guild_id:
+                    # Don't remove perms of everyone role
+                    permissions.append(permission)
                     break
+
+            await channel.modify(permission_overwrites=permissions)
             await guild_data.voice_lobbies.update()
 
     @command()
