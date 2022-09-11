71fce74a03e9a590565a0f91bc67635ee113914c
Damego <danyabatueff@gmail.com>
2022-09-11 14:49:02 +0500

    feat(vl): Add commands

--- source/extensions/voice_lobbies.py
+++ source/extensions/voice_lobbies.py
@@ -1,12 +1,20 @@
 from typing import Final
 
-from interactions import Channel, ChannelType, CommandContext, Extension, Overwrite, Permissions
+from interactions import (
+    Channel,
+    ChannelType,
+    CommandContext,
+    Extension,
+    Member,
+    Overwrite,
+    Permissions,
+)
 from interactions import extension_command as command
 from interactions import extension_listener as listener
 from interactions import option
 from interactions.ext.lavalink import VoiceState
 
-from core import Asteroid, GuildData, GuildVoiceLobbies  # isort: skip
+from core import Asteroid, BotException, GuildData, GuildVoiceLobbies  # isort: skip
 
 
 VOICE_CHANNEL_USER_PERMISSIONS: Final = (
@@ -57,11 +65,14 @@ class VoiceLobbies(Extension):
                 parent_id=voice_lobbies.category_channel_id,
                 permission_overwrites=permissions,
             )
-            await channel.send("There are some tips for you lmao.")  # TODO: Add text + localization
+            await channel.send(
+                "There are some tips for you lmao."
+            )  # TODO: Send a message with buttons & pin it
             await member.modify(guild_id=guild.id, channel_id=channel.id)
             voice_lobbies.add_channel(int(channel.id), int(member.id))
             await voice_lobbies.update()
             return
+
         await self.__remove_user_channel(before, after, guild_data)
         await self.__check_channels(voice_lobbies)
 
@@ -73,7 +84,7 @@ class VoiceLobbies(Extension):
         """
         if not before or not before.channel_id:
             return
-        lobby = guild_data.voice_lobbies.get_lobby(channel_id=int(before.channel_id))
+        lobby = guild_data.voice_lobbies.get_lobby(int(before.channel_id))
         if not lobby:
             return
         channel = await self.client.get_channel(before.channel_id)
@@ -124,7 +135,7 @@ class VoiceLobbies(Extension):
     @command()
     async def voice(self, ctx: CommandContext):
         """Base command"""
-        ...
+        await ctx.defer(ephemeral=True)
 
     @voice.subcommand()
     @option("Name for voice channel. Can be edited later.")
@@ -138,7 +149,6 @@ class VoiceLobbies(Extension):
         private_lobbies: bool = False,
     ):
         """Setup voice lobbies on your server"""
-        await ctx.defer(ephemeral=True)
         # TODO: Check author perms. Should be MANAGE_SERVER ig
         # guild_data = await self.client.database.get_guild(ctx.guild_id)
         guild = await self.client.get_guild(ctx.guild_id)
@@ -160,6 +170,135 @@ class VoiceLobbies(Extension):
             private_lobbies,
         )
         await ctx.send("Ready")
+
+    @voice.group()
+    async def lobby(self, ctx: CommandContext):
+        """Group command"""
+
+    @lobby.subcommand()
+    @option("The member to block")
+    async def block_member(self, ctx: CommandContext, member: Member):
+        """Blocks the member from your lobby"""
+        if ctx.author.id == member.id:
+            raise BotException(203)
+
+        channel = await self.__get_lobby_channel(ctx)
+        self.__set_overwrite(
+            channel.permission_overwrites,
+            id=int(member.id),
+            type=1,
+            deny=Permissions.VIEW_CHANNEL,
+            allow=0,
+            add_new_overwrite=True,
+        )
+
+        await channel.modify(permission_overwrites=channel.permission_overwrites)
+
+        await ctx.send(f"You blocked {member.mention}!")
+
+    @lobby.subcommand()
+    @option("The member to unblock")
+    async def unblock_member(self, ctx: CommandContext, member: Member):
+        """Unblocks member of your lobby"""
+        if ctx.author.id == member.id:
+            raise BotException(203)
+
+        channel = await self.__get_lobby_channel(ctx)
+        self.__set_overwrite(
+            channel.permission_overwrites,
+            id=int(member.id),
+            type=1,
+            deny=0,
+            allow=Permissions.VIEW_CHANNEL,
+            add_new_overwrite=True,
+        )
+
+        await channel.modify(permission_overwrites=channel.permission_overwrites)
+
+        await ctx.send(f"You unblocked {member.mention}!")
+
+    @lobby.subcommand()
+    @option("Hide?")
+    async def toggle_hidden(self, ctx: CommandContext, hide: bool):
+        """Toggle hidden status of your lobby"""
+        channel = await self.__get_lobby_channel(ctx)
+
+        allow = Permissions.VIEW_CHANNEL if not hide else 0
+        deny = Permissions.VIEW_CHANNEL if hide else 0
+        self.__set_overwrite(
+            channel.permission_overwrites, id=int(ctx.guild_id), type=0, deny=deny, allow=allow
+        )
+
+        await channel.modify(permission_overwrites=channel.permission_overwrites)
+
+        _hide_text = "was hided" if hide else "was unhide"
+        await ctx.send(f"Your lobby {_hide_text}!")
+
+    @lobby.subcommand()
+    @option("The new lobby name")
+    async def change_name(self, ctx: CommandContext, name: str):
+        """Changes name of your lobby"""
+        channel = await self.__get_lobby_channel(ctx)
+
+        await channel.modify(name=name)
+
+    @lobby.subcommand()
+    @option("The new owner of lobby")
+    async def transfer_ownership(self, ctx: CommandContext, member: Member):
+        """Changes name of your lobby"""
+        if ctx.author.id == member.id:
+            raise BotException(204)
+
+        channel = await self.__get_lobby_channel(ctx)
+        self.__set_overwrite(
+            channel.permission_overwrites,
+            id=int(ctx.author.id),
+            type=1,
+            deny=VOICE_CHANNEL_USER_PERMISSIONS,
+        )
+        self.__set_overwrite(
+            channel.permission_overwrites,
+            id=int(member.id),
+            type=1,
+            allow=VOICE_CHANNEL_USER_PERMISSIONS,
+        )
+
+        await channel.modify(permission_overwrites=channel.permission_overwrites)
+
+        guild_data = await self.client.database.get_guild(ctx.guild_id)
+        lobby = guild_data.voice_lobbies.get_lobby(int(channel.id))
+        lobby.owner_id = int(member.id)
+        await guild_data.voice_lobbies.update()
+
+    async def __get_lobby_channel(self, ctx: CommandContext) -> Channel:
+        guild_data = await self.client.database.get_guild(ctx.guild_id)
+        voice_lobbies = guild_data.voice_lobbies
+
+        if not voice_lobbies:
+            raise BotException(201)
+        user_lobby = voice_lobbies.get_lobby(owner_id=int(ctx.author.id))
+        if not user_lobby:
+            raise BotException(202)
+
+        return await self.client.get_channel(user_lobby.channel_id)
+
+    @staticmethod
+    def __set_overwrite(
+        overwrites: list[Overwrite],
+        *,
+        id: int,
+        type: int,
+        allow: Permissions | int = 0,
+        deny: Permissions | int = 0,
+        add_new_overwrite: bool = False,
+    ):
+        overwrite = Overwrite(id=id, type=type, allow=allow, deny=deny)
+        for i, overwrite in enumerate(overwrites):
+            if overwrite.id == id:
+                overwrites[i] = overwrite  # Mixins...
+        if not add_new_overwrite:
+            return
+        overwrites.append(overwrite)
 
 
 def setup(client):
