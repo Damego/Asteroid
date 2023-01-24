ec6aa48107a0d015c532e6cae49f88858cca52c4
Damego <danyabatueff@gmail.com>
2023-01-24 15:06:58 +0500

    refactor: remove comments from guild_requests

--- source/core/database/requests/guild_requests.py
+++ source/core/database/requests/guild_requests.py
@@ -14,9 +14,7 @@ class GuildRequests:
 
     async def get_guild_raw_data(self, guild_id: int) -> dict:
         main_collection = self.__get_collection(guild_id, "configuration")
-        # users_collection = self.__get_collection(guild_id, "users")
         data = [doc async for doc in main_collection.find()]
-        # users_data = [doc async for doc in users_collection.find()]
         full_data = {}
         for document in data:
             id = document["_id"]
@@ -28,7 +26,7 @@ class GuildRequests:
                     full_data[id] = document
                 case _:
                     full_data[id] = document
-        # full_data["users"] = users_data
+
         return full_data
 
     async def __get_document(self, *keys: str, data: dict) -> dict | None:
@@ -60,7 +58,7 @@ class GuildRequests:
 
     async def add_guild(self, guild_id: int) -> dict:
         data = {"_id": "configuration", "language": "en-US"}
-        await self.__insert_document(guild_id, "configuration", data=data)
+        await self.__insert_document(str(guild_id), "configuration", data=data)
         return data
 
     async def remove_guild(self, guild_id: int) -> None:
@@ -69,12 +67,12 @@ class GuildRequests:
 
     async def add_user(self, guild_id: int, user_id: int) -> dict:
         data = {"_id": str(user_id)}
-        await self.__insert_document(guild_id, "users", data=data)
+        await self.__insert_document(str(guild_id), "users", data=data)
         return data
 
     async def get_user(self, guild_id: int, user_id: int) -> dict | None:
         data = {"_id": str(user_id)}
-        return await self.__get_document(guild_id, "users", data=data)
+        return await self.__get_document(str(guild_id), "users", data=data)
 
     async def remove_user(self, guild_id: int, user_id: int) -> None:
         data = {"_id": str(user_id)}
