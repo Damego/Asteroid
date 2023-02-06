29477ea60df0bc9109c3f4ee4895c607ca3c214f
Damego <damego.dev@gmail.com>
2023-02-06 13:33:59 +0500

    refactor(decorators): use tuple in all

--- source/core/decorators.py
+++ source/core/decorators.py
@@ -3,4 +3,4 @@
 from interactions import extension_command as command
 from interactions import extension_listener as listener
 
-__all__ = ["command", "listener"]
+__all__ = ("command", "listener")
